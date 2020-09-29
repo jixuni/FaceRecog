9d75ee142373111f8c7d4950ebebc3e155536687
jixuni <jixuni@gmail.com>
2020-09-29 14:21:03 -0400

    added css and particles

--- src/App.js
+++ src/App.js
@@ -3,11 +3,25 @@ import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import Rank from "./components/Rank/Rank";
+import Particles from "react-particles-js";
 import "./App.css";
+
+const particlesOptions = {
+  particles: {
+    numbers: {
+      value: 30,
+      density: {
+        enable: true,
+        value_area: 800,
+      },
+    },
+  },
+};
 
 function App() {
   return (
     <div className="App">
+      <Particles className="particles" params={particlesOptions} />
       <Navigation />
       <Logo />
       <Rank />
