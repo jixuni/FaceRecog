8e9541a2572ec97f59c228e8ad4e3050fbe412cf
jixuni <jixuni@gmail.com>
2020-10-14 17:53:08 -0400

    aggregate backend

---
+++ src/schema/db.sql
@@ -0,0 +1,21 @@
+
+--Create table users
+CREATE TABLE users
+(
+  id serial PRIMARY KEY,
+  name varchar(100),
+  email TEXT UNIQUE NOT NULL,
+  entries bigint DEFAULT 0,
+  joined timestamp NOT NULL
+);
+
+--Create table login
+CREATE TABLE login
+(
+  id serial PRIMARY KEY,
+  hash varchar(100) NOT NULL,
+  email TEXT UNIQUE NOT NULL
+);
+
+
+
